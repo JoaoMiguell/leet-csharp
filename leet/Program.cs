7eeb27c6958690b4730fc55f7948a1cc91097aa3
João Miguel <joaomiguel_sm@outlook.com>
2023-04-12 21:12:40 -0300

    1. Two Sum

--- leet/Program.cs
+++ leet/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(TwoSum(new int[] {2, 7, 11, 15}, 9));
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] res = new int[2];
+            for(int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if(i != j)
+                    {
+                        if(nums[i] + nums[j] == target)
+                        {
+                            res[0] = i;
+                            res[1] = j;
+                        }
+                    }
+                }
+            }
+
+            return res.ToString;
         }
     }
 }
