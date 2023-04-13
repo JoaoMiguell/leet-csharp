0901386ab7e1af7744eb6706259bea32ddf6f2bd
João Miguel <joaomiguel_sm@outlook.com>
2023-04-13 00:19:44 -0300

    704. Binary Search

--- leet/Program.cs
+++ leet/Program.cs
@@ -4,18 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(IsPalindrome(121));
+            Console.WriteLine(Search(new int[] {-1, 0, 3, 5, 9, 12}, 9));
         }
 
-        public static bool IsPalindrome(int x)
+        public static int Search(int[] nums, int target)
         {
-            char[] reverse = x.ToString().ToCharArray();
-            Array.Reverse(reverse);
+            int result = -1;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] == target)
+                    result = i;
+            }
 
-            if(new string(reverse) == x.ToString())
-                return true;
-            else
-                return false;
+            return result;
         }
     }
 }
