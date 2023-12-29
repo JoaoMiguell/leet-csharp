8d355eac2adc4eee4b6ffaddea8ba8b3e8077f14
Joao Miguell <joaomiguel_sm@outlook.com>
2023-12-29 19:36:43 -0300

    Romam to integer

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,24 +1,20 @@
-﻿public class ListNode {
-  public int val;
-  public ListNode next;
-  public ListNode(int val = 0, ListNode next = null) {
-    this.val = val;
-    this.next = next;
-  }
-}
-
-public class Program {
+﻿public class Program {
   public static void Main() {
-    Console.WriteLine(IsPalindrome(10));
+    Console.WriteLine(RomanToInt("LVIII"));
   }
 
-  static bool IsPalindrome(int x) {
-    int iNum = 0;
-    int cX = x;
-    while(cX > 0) {
-      iNum = (iNum*10) + (cX % 10);
-      cX /= 10;
-    }
-    return x == iNum;
+  static int RomanToInt(string s) {
+    Dictionary<char, int> map = new() {
+      {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+      {'C', 100}, {'D', 500}, {'M', 1000}
+    };
+    int res = 0;
+    for(int i = 0; i < s.Length; i++) {
+      if(i + 1 < s.Length && map[s[i]] < map[s[i + 1]])
+        res -= map[s[i]];
+      else
+        res += map[s[i]];
+      }
+    return res;
   }
 }
