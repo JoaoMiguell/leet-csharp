9961cbdefbe0f6010a2dec61d44dd1f6abcb7475
Joao Miguell <joaomiguel_sm@outlook.com>
2023-12-30 02:34:10 -0300

    Merge Two Sorted Lists

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,15 +1,46 @@
-﻿public class Program {
+﻿public class ListNode {
+  public int val;
+  public ListNode next;
+  public ListNode(int val = 0, ListNode next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+public class Program {
   public static void Main() {
-    Console.WriteLine(IsValid("]"));
+    ListNode res = MergeTwoLists(
+      new ListNode(1, new ListNode(2, new ListNode(4))),
+      new ListNode(1, new ListNode(3, new ListNode(4)))
+    );
+    while(res != null) {
+      Console.WriteLine(res.val);
+      res = res.next;
+    }
   }
 
-  static bool IsValid(string s) {
-    Dictionary<char, char> map = new() { {'}', '{'}, {')', '('}, {']', '['} };
-    Stack<char> stack = new();
-    foreach(char c in s) {
-      if(map.ContainsKey(c) && stack.Count > 0 && map[c] == stack.Peek()) stack.Pop();
-      else stack.Push(c);
+  static ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+    if(list1 == null) return list2;
+    if(list2 == null) return list1;
+
+    List<int> values = [];
+    while(list1 != null) {
+      values.Add(list1.val);
+      list1 = list1.next;
     }
-    return stack.Count == 0;
+    while(list2 != null) {
+      values.Add(list2.val);
+      list2 = list2.next;
+    }
+
+    values.Sort();
+
+    ListNode result = new(values[0]);
+    ListNode ptr = result;
+    for(int i = 1; i < values.Count; i++) {
+      ptr.next = new(values[i]);
+      ptr = ptr.next;
+    }
+    return result;
   }
 }
