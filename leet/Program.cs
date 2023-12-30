3acb08feb956c33448a607a74317091c5a34ab76
Joao Miguell <joaomiguel_sm@outlook.com>
2023-12-30 18:36:19 -0300

    Search Insert Position

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,9 +1,18 @@
 ﻿public class Program {
   public static void Main() {
-    Console.WriteLine(StrStr("sadbutsad", "sad"));
+    Console.WriteLine(SearchInsert([-1, 3, 5, 6], 0));
   }
 
-  static int StrStr(string haystack, string needle) {
-    return haystack.IndexOf(needle);
+  static int SearchInsert(int[] nums, int target) {
+    if(target == 0 && nums[0] > 0) return 0;
+    if(target < nums[0]) return 0;
+    for(int i = 0; i < nums.Length; i++) {
+      int diff = target - nums[i];
+      if(diff == 0) return i;
+      if(diff == 1) return i + 1;
+      if(diff ==-1) return i;
+    }
+
+    return nums.Length;
   }
 }
