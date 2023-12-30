e254b2fb957d96b63e14ac379ea705a466398ffe
Joao Miguell <joaomiguel_sm@outlook.com>
2023-12-30 17:58:25 -0300

    RemoveDuplicates

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,46 +1,11 @@
-﻿public class ListNode {
-  public int val;
-  public ListNode next;
-  public ListNode(int val = 0, ListNode next = null) {
-    this.val = val;
-    this.next = next;
-  }
-}
-
-public class Program {
+﻿public class Program {
   public static void Main() {
-    ListNode res = MergeTwoLists(
-      new ListNode(1, new ListNode(2, new ListNode(4))),
-      new ListNode(1, new ListNode(3, new ListNode(4)))
-    );
-    while(res != null) {
-      Console.WriteLine(res.val);
-      res = res.next;
-    }
+    Console.WriteLine(RemoveDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
   }
 
-  static ListNode MergeTwoLists(ListNode list1, ListNode list2) {
-    if(list1 == null) return list2;
-    if(list2 == null) return list1;
-
-    List<int> values = [];
-    while(list1 != null) {
-      values.Add(list1.val);
-      list1 = list1.next;
-    }
-    while(list2 != null) {
-      values.Add(list2.val);
-      list2 = list2.next;
-    }
-
-    values.Sort();
-
-    ListNode result = new(values[0]);
-    ListNode ptr = result;
-    for(int i = 1; i < values.Count; i++) {
-      ptr.next = new(values[i]);
-      ptr = ptr.next;
-    }
-    return result;
+  static int RemoveDuplicates(int[] nums) {
+    List<int> list = nums.Distinct().ToList();
+    for(int i = 0; i < list.Count; i++) nums[i] = list[i];
+    return list.Count;
   }
 }
