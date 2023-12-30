7826fd15d5c39a9e2fa69756a9d3cf07ee755f12
Joao Miguell <joaomiguel_sm@outlook.com>
2023-12-30 18:01:46 -0300

    Remove Element

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,10 +1,10 @@
 ﻿public class Program {
   public static void Main() {
-    Console.WriteLine(RemoveDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+    Console.WriteLine(RemoveElement([3, 2, 2, 3], 3));
   }
 
-  static int RemoveDuplicates(int[] nums) {
-    List<int> list = nums.Distinct().ToList();
+  static int RemoveElement(int[] nums, int val) {
+    List<int> list = nums.Where(num => num != val).ToList();
     for(int i = 0; i < list.Count; i++) nums[i] = list[i];
     return list.Count;
   }
