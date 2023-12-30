8d57a0719ef0f3eafc1350054da39f696dcb0bc5
Joao Miguell <joaomiguel_sm@outlook.com>
2023-12-30 18:10:02 -0300

    Find the Index of the First Occurrence in a String

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,11 +1,9 @@
 ﻿public class Program {
   public static void Main() {
-    Console.WriteLine(RemoveElement([3, 2, 2, 3], 3));
+    Console.WriteLine(StrStr("sadbutsad", "sad"));
   }
 
-  static int RemoveElement(int[] nums, int val) {
-    List<int> list = nums.Where(num => num != val).ToList();
-    for(int i = 0; i < list.Count; i++) nums[i] = list[i];
-    return list.Count;
+  static int StrStr(string haystack, string needle) {
+    return haystack.IndexOf(needle);
   }
 }
