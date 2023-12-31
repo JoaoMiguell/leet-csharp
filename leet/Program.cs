60fe577ad754353478905071614a169f57434737
Joao Miguell <joaomiguel_sm@outlook.com>
2023-12-30 23:49:03 -0300

    Plus One

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,10 +1,18 @@
 ﻿public class Program {
   public static void Main() {
-    Console.WriteLine(LengthOfLastWord("   fly me   to   the moon  "));
+    Console.WriteLine(PlusOne([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
   }
 
-  static int LengthOfLastWord(string s) {
-    string[] words = s.Trim().Split(' ');
-    return words[words.Length-1].Length;
+  static int[] PlusOne(int[] digits) {
+    for(int i = digits.Length - 1; i >= 0; i--) {
+      if(digits[i] < 9) {
+        digits[i]++;
+        return digits;
+      }
+      digits[i] = 0;
+    }
+    int[] res = new int[digits.Length+1];
+    res[0] = 1;
+    return res;
   }
 }
