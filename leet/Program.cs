359205aa01a195ef09811a047dc6b1b11709c89e
Joao Miguell <joaomiguel_sm@outlook.com>
2023-12-30 23:14:38 -0300

    Length of Last Word

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,18 +1,10 @@
 ﻿public class Program {
   public static void Main() {
-    Console.WriteLine(SearchInsert([-1, 3, 5, 6], 0));
+    Console.WriteLine(LengthOfLastWord("   fly me   to   the moon  "));
   }
 
-  static int SearchInsert(int[] nums, int target) {
-    if(target == 0 && nums[0] > 0) return 0;
-    if(target < nums[0]) return 0;
-    for(int i = 0; i < nums.Length; i++) {
-      int diff = target - nums[i];
-      if(diff == 0) return i;
-      if(diff == 1) return i + 1;
-      if(diff ==-1) return i;
-    }
-
-    return nums.Length;
+  static int LengthOfLastWord(string s) {
+    string[] words = s.Trim().Split(' ');
+    return words[words.Length-1].Length;
   }
 }
