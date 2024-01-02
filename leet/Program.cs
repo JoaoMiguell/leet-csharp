a618e7337cf371e012847e61930c97a8fb5c2075
Joao Miguell <joaomiguel_sm@outlook.com>
2024-01-02 17:28:18 -0300

    2610. Convert an Array Into a 2D Array With Conditions

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,31 +1,31 @@
-﻿public class ListNode {
-  public int val;
-  public ListNode next;
-  public ListNode(int val = 0, ListNode next = null) {
-    this.val = val;
-    this.next = next;
-  }
-}
-
-public class Program {
+﻿public class Program {
   public static void Main() {
-    Console.WriteLine(DeleteDuplicates(new ListNode(1,new ListNode(1, new ListNode(2)))));
+    var r = FindMatrix([1, 3, 4, 1, 2, 3, 1]);
+
+    foreach(var item in r) {
+      foreach(var item1 in item) {
+        Console.Write($"{item1} ");
+      }
+      Console.WriteLine();
+    }
   }
 
-  static ListNode DeleteDuplicates(ListNode head) {
-    HashSet<int> ints = new();
-    while(head != null) {
-      if(!ints.Contains(head.val)) ints.Add(head.val);
-      head = head.next;
+  static IList<IList<int>> FindMatrix(int[] nums) {
+    List<int> lNums = [.. nums];
+    IList<IList<int>> res = new List<IList<int>>();
+
+    while(lNums.Count > 0) {
+      List<int> temp = [];
+      for(int j = 0; j < lNums.Count; j++) {
+        if(!temp.Contains(lNums[j])) {
+          temp.Add(lNums[j]);
+          lNums.RemoveAt(j);
+          j--;
+        }
+      }
+      res.Add(temp);
     }
 
-    ListNode res = new();
-    ListNode ptrRes = res;
-    foreach(int i in ints) {
-      res.next = new ListNode(i);
-      res = res.next;
-    }
-
-    return ptrRes.next;
+    return res;
   }
 }
