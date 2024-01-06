1c6b9cfd288ea10aa7327209e8d6888fc7fa2f67
Joao Miguell <joaomiguel_sm@outlook.com>
2024-01-06 19:54:57 -0300

    1235. Maximum Profit in Job Scheduling

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,18 +1,29 @@
 ﻿public class Program {
   public static void Main() {
-    Console.WriteLine(LengthOfLIS([0, 1, 0, 3, 2, 3]));
+    Console.WriteLine(JobScheduling([1, 2, 3, 3], [3, 4, 5, 6], [50, 10, 40, 70]));
   }
 
-  static int LengthOfLIS(int[] nums) {
-    int[] ints = new int[nums.Length];
-    Array.Fill(ints, 1);
+  static int JobScheduling(int[] startTime, int[] endTime, int[] profit) {
+    int n = startTime.Length;
+    List<Job> jobs = new();
 
-    for(int i = nums.Length - 1;  i >= 0; i--) {
-      for(int j = i+1; j < nums.Length; j++) {
-        if(nums[i] < nums[j]) ints[i] = Math.Max(ints[i], 1 + ints[j]);
+    for(int i = 0; i < n; i++)
+      jobs.Add(new Job(startTime[i], endTime[i], profit[i]));
+
+    jobs.Sort((x, y) => x.EndTime.CompareTo(y.EndTime));
+    int[] dp = new int[n+1];
+    
+    for(int i = 1; i <= n; i++) {
+      dp[i] = Math.Max(dp[i - 1], jobs[i - 1].Profit);
+      for(int j = i -1; j > 0; j--) {
+        if(jobs[i-1].StartTime >= jobs[j-1].EndTime) {
+          dp[i] = Math.Max(dp[i], dp[j] + jobs[i - 1].Profit);
+          break;
+        }
       }
     }
-
-    return ints.Max();
+    return dp[n];
   }
 }
+
+public record Job(int StartTime, int EndTime, int Profit);
