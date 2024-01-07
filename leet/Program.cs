c8bcd9fc4e0f3741f6811de917ed441c01c182e7
Joao Miguell <joaomiguel_sm@outlook.com>
2024-01-07 16:02:08 -0300

    392. Is Subsequence

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,29 +1,14 @@
 ﻿public class Program {
   public static void Main() {
-    Console.WriteLine(JobScheduling([1, 2, 3, 3], [3, 4, 5, 6], [50, 10, 40, 70]));
+    Console.WriteLine(IsSubsequence("ab", "baab"));
   }
 
-  static int JobScheduling(int[] startTime, int[] endTime, int[] profit) {
-    int n = startTime.Length;
-    List<Job> jobs = new();
-
-    for(int i = 0; i < n; i++)
-      jobs.Add(new Job(startTime[i], endTime[i], profit[i]));
-
-    jobs.Sort((x, y) => x.EndTime.CompareTo(y.EndTime));
-    int[] dp = new int[n+1];
-    
-    for(int i = 1; i <= n; i++) {
-      dp[i] = Math.Max(dp[i - 1], jobs[i - 1].Profit);
-      for(int j = i -1; j > 0; j--) {
-        if(jobs[i-1].StartTime >= jobs[j-1].EndTime) {
-          dp[i] = Math.Max(dp[i], dp[j] + jobs[i - 1].Profit);
-          break;
-        }
-      }
+  static bool IsSubsequence(string s, string t) {
+    int i = 0, j = 0;
+    while (i < s.Length && j < t.Length) {
+      if(s[i] == t[j]) i++;
+      j++;
     }
-    return dp[n];
+    return i == s.Length;
   }
 }
-
-public record Job(int StartTime, int EndTime, int Profit);
