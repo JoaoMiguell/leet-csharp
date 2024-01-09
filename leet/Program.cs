b541ad889668f4e20523d0a0ced63e0220076b6f
Joao Miguell <joaomiguel_sm@outlook.com>
2024-01-09 10:18:55 -0300

    1431. Kids With the Greatest Number of Candies

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,16 +1,14 @@
 ﻿public class Program {
   public static void Main() {
-    Console.WriteLine(GcdOfStrings("ABCABC", "ABC"));
+    var res = KidsWithCandies([2, 3, 5, 1, 3], 3);
+    foreach(var item in res)
+      Console.WriteLine(item);
   }
 
-  static string GcdOfStrings(string str1, string str2) {
-    int a = str1.Length, b = str2.Length;
-    if(!(str1 + str2).Equals(str2 + str1)) return "";
-    while(b != 0) {
-      int temp = a % b;
-      a = b;
-      b = temp;
-    }
-    return str1[..a];
+  static IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
+    bool[] res = new bool[candies.Length];
+    for(int i = 0; i < candies.Length; i++)
+      if(candies[i] + extraCandies >= candies.Max()) res[i] = true;
+    return res;
   }
 }
