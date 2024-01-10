e324e233d3b927c43d38f2376516af80cc472c9d
Joao Miguell <joaomiguel_sm@outlook.com>
2024-01-10 20:50:37 -0300

    605. Can Place Flowers

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,36 +1,19 @@
-﻿public class TreeNode {
-  public int val;
-  public TreeNode left;
-  public TreeNode right;
-  public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null) {
-    this.val = val;
-    this.left = left;
-    this.right = right;
-  }
-}
-
-public class Program {
+﻿public class Program {
   public static void Main() {
-    Console.WriteLine(LeafSimilar(
-      new TreeNode(3, new TreeNode(5, new TreeNode(6), new TreeNode(2, new TreeNode(7), new TreeNode(4))), new TreeNode(1, new TreeNode(9), new TreeNode(8))),
-      new TreeNode(3, new TreeNode(5, new TreeNode(6), new TreeNode(2, new TreeNode(7), new TreeNode(4))), new TreeNode(1, new TreeNode(9), new TreeNode(8)))
-      ));
+    Console.WriteLine(CanPlaceFlowers([0, 0, 1, 0, 0], 1));
   }
 
-  static bool LeafSimilar(TreeNode root1, TreeNode root2) {
-    List<int> leaf1 = new(), leaf2 = new();
-    Dfs(root1, leaf1);
-    Dfs(root2, leaf2);
-    return leaf1.SequenceEqual(leaf2);
-  }
+  static bool CanPlaceFlowers(int[] flowerbed, int n) {
+    int[] temp = [0, ..flowerbed, 0];
+    int res = 0;
 
-  static void Dfs(TreeNode root, List<int> leaf) {
-    if(root == null) return;
-    if(root.left == null && root.right == null) {
-      leaf.Add(root.val);
-      return;
+    for(int i = 1; i < temp.Length-1; i++) {
+      if(temp[i-1] == 0 && temp[i] == 0 && temp[i+1] == 0) {
+        temp[i] = 1;
+        res++;
+      }
     }
-    Dfs(root.left, leaf);
-    Dfs(root.right, leaf);
+
+    return res >= n;
   }
 }
