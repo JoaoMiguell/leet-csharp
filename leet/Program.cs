16bfdd79c4f6f95ad83f6f50a8a99c6273314e2d
Joao Miguell <joaomiguel_sm@outlook.com>
2024-01-11 19:49:55 -0300

    151. Reverse Words in a String

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,17 +1,9 @@
 ﻿public class Program {
   public static void Main() {
-    Console.WriteLine(ReverseVowels("hello"));
+    Console.WriteLine(ReverseWords("a good   example"));
   }
 
-  static string ReverseVowels(string s) {
-    string res = "", comp = "aeiouAEIOU";
-    Stack<char> stack = new(s.Where(x => comp.Contains(x)));
-
-    foreach(char c in s) {
-      if(comp.Contains(c)) res += stack.Pop();
-      else res += c;
-    }
-
-    return res;
+  static string ReverseWords(string s) {
+    return string.Join(" ", s.Trim().Split(' ').Where(s => s != "").ToArray().Reverse()); 
   }
 }
