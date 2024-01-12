d4f5c26ce74396391ac9899a664be3d65d9dbca9
Joao Miguell <joaomiguel_sm@outlook.com>
2024-01-11 22:54:07 -0300

    238. Product of Array Except Self

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,9 +1,20 @@
 ﻿public class Program {
   public static void Main() {
-    Console.WriteLine(ReverseWords("a good   example"));
+    Console.WriteLine(string.Join(",", ProductExceptSelf([1, 2, 3, 4])));
   }
 
-  static string ReverseWords(string s) {
-    return string.Join(" ", s.Trim().Split(' ').Where(s => s != "").ToArray().Reverse()); 
+  static int[] ProductExceptSelf(int[] nums) {
+    List<int> res = [];
+    int carry = 1;
+    for(int i = 0; i < nums.Length; i++) {
+      res.Add(carry);
+      carry *= nums[i];
+    }
+    carry = 1;
+    for(int i = nums.Length - 1; i >= 0; i--) {
+      res[i] *= carry;
+      carry *= nums[i];
+    }
+    return [.. res];
   }
 }
