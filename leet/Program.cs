c5ee7af1b9e8477b0e31c20a1fd2b53ab0caf406
Joao Miguell <joaomiguel_sm@outlook.com>
2024-01-12 22:44:00 -0300

    443. String Compression

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,18 +1,24 @@
 ﻿public class Program {
   public static void Main() {
-    Console.WriteLine(IncreasingTriplet([5, 4, 3, 2, 1]));
+    Console.WriteLine(Compress(['a', 'a', 'a', 'b', 'b', 'a', 'a']));
   }
 
-  static bool IncreasingTriplet(int[] nums) {
-    int val1 = int.MaxValue, val2 = int.MaxValue;
-    for(int i = 0; i < nums.Length; i++) {
-      if(nums[i] <= val1)
-        val1 = nums[i];
-      else if(nums[i] <= val2)
-        val2 = nums[i];
-      else
-        return true;
+  static int Compress(char[] chars) {
+    int i = 0, res = 0;
+    while(i < chars.Length) {
+      int sameLength = 1;
+      while(i + sameLength < chars.Length && chars[i + sameLength] == chars[i])
+        sameLength++;
+
+      chars[res++] = chars[i];
+      if(sameLength > 1) {
+        foreach(var c in sameLength.ToString())
+          chars[res++] = c;
+      }
+
+      i += sameLength;
     }
-    return false;
+
+    return res;
   }
 }
