b68bf8d5dbcffcd1fd249721f0e2757c4d51aef3
Joao Miguell <joaomiguel_sm@outlook.com>
2024-01-18 23:28:39 -0300

    643. Maximum Average Subarray I

--- leet/Program.cs
+++ leet/Program.cs
@@ -1,20 +1,24 @@
 ﻿public class Program {
   public static void Main() {
-    Console.WriteLine(MaxOperations([1, 2, 3, 4],5));
+    Console.WriteLine(FindMaxAverage([1, 12, -5, -6, 50, 3], 4));
   }
 
-  static int MaxOperations(int[] nums, int k) {
-    int res = 0, l = 0, r = nums.Length-1;
-    while(l < r) {
-      int sum = nums[l] + nums[r];
-      if(sum == k) {
-        res++;
-        l++;
-        r--;
-      }
-      else if(sum < k)l++;
-      else r--;
+  static double FindMaxAverage(int[] nums, int k) {
+    //double sum = (double)nums.Take(k).Sum();
+    //int skip = 1;
+    //for(int i = k; i < nums.Length; i++) {
+    //  double temp = (double)nums.Skip(skip).Take(k).Sum();
+    //  sum = Math.Max(sum, temp);
+    //  skip++;
+    //}
+    //return sum/k;
+    double sum = 0;
+    for(int i = 0; i < k; i++) sum += nums[i];
+    double curr = sum;
+    for(int i = k; i < nums.Length; i++) {
+      curr += nums[i] - nums[i - k];
+      sum = Math.Max(sum, curr);
     }
-    return res;
+    return sum/k;
   }
 }
